98b5f488bce6aa3fddc4ec05e4d3cd0d34f2a2db
saelsa <saelsa@gmx.net>
2018-05-25 15:19:26 +0800

    add portfolio page

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(function () {
+
+    //typed.js type-writer effect
+    //credit to www.mattboldt.com
+    $(".write").typed({
+        strings: ["Data visualisation", "Wordpress", "Web Development"],
+        typeSpeed: 40,
+        smartBackspace: true,
+        backSpeed: 40
+    });
+
+    //activate animate.css effects on scrolling using waypoints
+    //credit to http://imakewebthings.com/waypoints/
+    $('.js__section-contact').waypoint(function (direction) {
+        $('.js__social-item').removeClass('invisible');
+        $('.js__social-item').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+
+    //smooth-scrolling effect
+    //credit to https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    //Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+
+});
